c17f6a4463ce50421c4fa136b91e2d7fd0b3bec8
uni51 <unieye51@gmail.com>
2024-05-09 08:58:33 +0900

    33. 認証ページに遷移するためのページを作ろう

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <h2>新規登録</h2>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">登録</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
