15031df4027b4a6e25a3d413299f63c9e7f57c13
uni51 <unieye51@gmail.com>
2024-05-10 08:53:54 +0900

    36. Propsを付与してコンポーネントを受け取る側で動的に変更させよう

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -6,9 +6,24 @@ const Signup = () => {
     <div className="mx-auto max-w-sm my-14">
       <h2 className="text-center font-medium text-2xl mb-4">新規登録</h2>
       <form>
-        <InputField />
-        <InputField />
-        <InputField />
+        <InputField
+          label="ユーザー名"
+          name="username"
+          type="text"
+          placeholder="ユーザー名"
+        />
+        <InputField
+          label="メールアドレス"
+          name="email"
+          type="email"
+          placeholder="メールアドレス"
+        />
+        <InputField
+          label="パスワード"
+          name="password"
+          type="password"
+          placeholder="パスワード"
+        />
         <div className="mt-4">
           <button type="submit">登録</button>
         </div>
--- app/features/auth/components/InputField.tsx
+++ app/features/auth/components/InputField.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 
-const InputField = () => {
+interface InputFieldProps {
+  name: string;
+  label: string;
+  type: string;
+  placeholder?: string;
+}
+
+const InputField = ({ name, label, type, placeholder }: InputFieldProps) => {
   return (
     <div>
       <label
-        htmlFor="name"
+        htmlFor={name}
         className="text-gray-700 text-sm font-bold mb-1 block mt-4"
       >
-        ユーザー名
+        {label}
       </label>
       <input
-        type="text"
-        id="name"
-        placeholder="ユーザー名"
+        type={type}
+        id={name}
+        placeholder={placeholder}
         className="border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none"
       />
     </div>
